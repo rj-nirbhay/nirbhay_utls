214faf87e5824ca3b338f39cc5abb230344c3a9e
rj-nirbhay <53961074+rj-nirbhay@users.noreply.github.com>
2020-07-19 20:57:15 +0530

    Update Update_R.R

--- Update_R.R
+++ Update_R.R
@@ -5,7 +5,7 @@ install.packages('devtools') #assuming it is not already installed
 library(devtools)
 install_github('andreacirilloac/updateR')
 library(updateR)
-updateR(admin_password = 2345)
+updateR(admin_password = ****)
 # windows
 install.packages("installr")
 library(installr)
