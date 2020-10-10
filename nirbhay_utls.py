72c318f37e28acdd3fbdb7ca9d7d507c14351e03
rj-nirbhay <53961074+rj-nirbhay@users.noreply.github.com>
2020-10-10 11:49:02 +0530

    added f-test

--- nirbhay_utls.py
+++ nirbhay_utls.py
@@ -563,5 +563,29 @@ def rem_xpctl_more(data_col,pctl_th=0.99):
     return data_col>pctl
 
 # -----------------------------------------------------------------------------------------------------------------
-# Function 17: 
+# Function 17: F-test
 # -----------------------------------------------------------------------------------------------------------------
+import numpy as np
+from scipy import stats
+import pandas as pd
+
+#define F-test function
+def f_test(x, y):
+    x = np.array(x)
+    y = np.array(y)
+    f = np.var(x, ddof=1)/np.var(y, ddof=1) #calculate F test statistic 
+    dfn = x.size-1 #define degrees of freedom numerator 
+    dfd = y.size-1 #define degrees of freedom denominator 
+    p = 1-scipy.stats.f.cdf(f, dfn, dfd) #find p-value of F test statistic 
+    return f, p
+
+#perform F-test
+# f_test(df['var1'],df['var2'])
+
+# -----------------------------------------------------------------------------------------------------------------
+# Function 18:
+# -----------------------------------------------------------------------------------------------------------------
+
+
+
+
