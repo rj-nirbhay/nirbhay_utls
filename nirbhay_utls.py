958df34c7ecb702857fe47ad408f0f737934f3f3
rj-nirbhay <53961074+rj-nirbhay@users.noreply.github.com>
2020-10-14 10:07:50 +0530

    added decile wise event rate plot

--- nirbhay_utls.py
+++ nirbhay_utls.py
@@ -592,7 +592,34 @@ def variance_threshold_selector(data, threshold=0.5):
     return data[data.columns[selector.get_support(indices=True)]]
 
 # -----------------------------------------------------------------------------------------------------------------
-# Function 19:
+# Function 19: Decile wise Event rate and population plot for logistic regression
 # -----------------------------------------------------------------------------------------------------------------
+def decile_event_rate_plot(ds,Population = 'Population_%',Event_rate ='Event_rate',decile ='Band',sample_type ='Development'):
+    """
+    function that plot band wise event rate and population
+    args:
+        ds : dataset with population, band, event_rate
+        Population : str, feature name for population
+        Event_rate : str, feature name for Event_rate
+        decile : str, feature name for decile/bands
+        sample_type : str, feature name for sample_type , used in title of the plot
+        
+    returns: plot
+    """
+    import matplotlib.pyplot as plt
+    
+    fig, ax1 = plt.subplots(figsize=(8, 4))
+    ds[Population].plot(kind='bar', color='b', width = 0.35,legend=True , label = Population)
+    ds[Event_rate].plot(kind='line',color ='r', secondary_y=True,legend=True, label = Event_rate)
+    ax1.set_xticklabels(ds[decile])
+    ax1.set_ylim(0,ds[Event_rate].max()*0.15)
+    ax1.right_ax.set_ylim(0,ds[Event_rate].max()*1.5)
+    ax1.right_ax.set_ylabel(Event_rate)
+    ax1.set_ylabel(Population)
+    plt.title('Decile Wise Event Rate :' +str(sample_type) +' sample')
+    ax1.set_xlabel(decile)
+    plt.show()
 
-
+# -----------------------------------------------------------------------------------------------------------------
+# Function 20:
+# -----------------------------------------------------------------------------------------------------------------
